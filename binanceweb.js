d8cb5a41e5942fe40edaddbefaeedf1be6aeb552
tinyult <tinyUlt@icloud.com>
2019-06-18 17:31:51 +0800

    a

--- binanceweb.js
+++ binanceweb.js
@@ -125,7 +125,7 @@ function handle(pathName, req, response) {
     }
 
 }
-let server = app.listen(8081, function () {
+let server = app.listen(8080, function () {
 
     let host = server.address().address;
     let port = server.address().port;
